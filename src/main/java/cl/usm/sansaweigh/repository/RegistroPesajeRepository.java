95e611c3c1575cf7f02a201b5129abf7e19a293a
Ameli <4mmm3l14@gmail.com>
2026-06-29 22:17:20 -0400

    Creacion endpoints de la API REST

--- src/main/java/cl/usm/sansaweigh/repository/RegistroPesajeRepository.java
+++ src/main/java/cl/usm/sansaweigh/repository/RegistroPesajeRepository.java
@@ -4,6 +4,10 @@ import cl.usm.sansaweigh.model.RegistroPesaje;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface RegistroPesajeRepository extends MongoRepository<RegistroPesaje, String> {
+    List<RegistroPesaje> findByCreatedAtBetween(LocalDateTime inicio, LocalDateTime fin);
 }
